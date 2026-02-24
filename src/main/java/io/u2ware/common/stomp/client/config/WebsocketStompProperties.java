52395210a278f40a32fc034a9bd35ce0db8558d3
u2waremanager@gamil.com <u2waremanager@gamil.com>
2026-02-24 11:04:31 +0900

    u2ware-common-stomp

--- src/main/java/io/u2ware/common/stomp/client/config/WebsocketStompProperties.java
+++ src/main/java/io/u2ware/common/stomp/client/config/WebsocketStompProperties.java
@@ -2,23 +2,14 @@ package io.u2ware.common.stomp.client.config;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "io.u2ware.common.stomp")
 public class WebsocketStompProperties {
 
-    private String url ;
-    private String destination = "/"+UUID.randomUUID().toString();
+    private String url;
     private Map<String,String> subscriptions = new HashMap<>();
-
-    public String getDestination() {
-        return destination;
-    }
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
 
     public Map<String,String> getSubscriptions() {
         return subscriptions;
